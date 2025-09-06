9b05f86279a46458f28406b6a200e5b454f9cf52
Otis Opoku <otisopoku30@gmail.com>
2025-09-06 14:23:36 +0000

    create simple turtle racing game

---
+++ Day19/Day19c.py
@@ -0,0 +1,37 @@
+# simulate a simple turtle racing game
+
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make you bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles: list[Turtle] = []
+
+
+for turtle_index in range(0, len(colors)):
+    t = Turtle(shape="turtle")
+    t.color(colors[turtle_index])
+    t.penup()
+    t.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(t)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've Lost! The {winning_color} turtle is the winner!")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
+screen.exitonclick()
