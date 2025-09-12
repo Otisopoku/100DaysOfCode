cc827c1326a75e39fa5bafb37422518598023756
Otis Opoku <otisopoku30@gmail.com>
2025-09-12 15:42:25 +0000

    modularize code

--- Day20/snake.py
+++ Day20/snake.py
@@ -1,42 +1,48 @@
 from turtle import Turtle
+from enum import Enum
 
-SNAKE_POSITIONS = [(0,0), (-20, 0), (-40, 0)]
+SNAKE_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
-HEADING_POSITIONS = {"north": 90, "south": 270, "east": 0, "west": 180}
+
+class Direction(Enum):
+    UP = 90
+    DOWN = 270
+    LEFT = 180
+    RIGHT = 0
 
 class Snake:
-
     def __init__(self):
         self.snake_segments: list[Turtle] = []
 
         for position in SNAKE_POSITIONS:
-            segment1 = Turtle("square")
-            segment1.color("white")
-            segment1.penup()
-            segment1.goto(position)
-            self.snake_segments.append(segment1)
+            segment = Turtle("square")
+            segment.color("white")
+            segment.penup()
+            segment.goto(position)
+            self.snake_segments.append(segment)
+
+        self.head = self.snake_segments[0]
 
     def move(self):
-        for i in range(len(self.snake_segments) -1, 0, -1):
+        for i in range(len(self.snake_segments) - 1, 0, -1):
             new_x = self.snake_segments[i - 1].xcor()
             new_y = self.snake_segments[i - 1].ycor()
-            
-            self.snake_segments[i].penup()
             self.snake_segments[i].goto(new_x, new_y)
-        self.snake_segments[0].forward(MOVE_DISTANCE)
+
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.snake_segments[0].heading() != 270:
-            self.snake_segments[0].setheading(90)
+        if self.head.heading() != Direction.DOWN.value:
+            self.head.setheading(Direction.UP.value)
 
     def down(self):
-        if self.snake_segments[0].heading() != 90:
-            self.snake_segments[0].setheading(270)
+        if self.head.heading() != Direction.UP.value:
+            self.head.setheading(Direction.DOWN.value)
 
     def left(self):
-        if self.snake_segments[0].heading() != 0:
-            self.snake_segments[0].setheading(180)
+        if self.head.heading() != Direction.RIGHT.value:
+            self.head.setheading(Direction.LEFT.value)
 
     def right(self):
-        if self.snake_segments[0].heading() != 180:
-            self.snake_segments[0].setheading(0)
+        if self.head.heading() != Direction.LEFT.value:
+            self.head.setheading(Direction.RIGHT.value)
