1f7d127a3f0383a553b68ac54bd521910f069e34
Otis Opoku <otisopoku30@gmail.com>
2026-01-03 23:44:25 +0000

    save credentials to a file

--- Day29/main.py
+++ Day29/main.py
@@ -5,6 +5,18 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    
+    with open("data.txt", "a") as file:
+        file.write(f"{website} | {email} | {password} \n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+        
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -29,8 +41,10 @@ password_label.grid(row=3, column=0)
 
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "otisopoku30@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
@@ -38,7 +52,7 @@ password_entry.grid(row=3, column=1)
 
 generate_pass_button = Button(text="Generate Password")
 generate_pass_button.grid(row=3, column=2)
-add_btn = Button(text="Add", width=36)
+add_btn = Button(text="Add", width=36, command=save)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
