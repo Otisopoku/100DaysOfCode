64c6e3967ec62fc116fe56be43b48b656bb97800
Otis Opoku <otisopoku30@gmail.com>
2026-01-14 18:00:06 +0000

    UI set up

---
+++ Day31/main.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# canvas object enables us to lay objects ontop of each other
+canvas = Canvas(width=800, height=526)
+front_card_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 265, image=front_card_img)
+canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
+
+right_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_image, highlightthickness=0)
+right_button.grid(row=1, column=1)
+
+window.mainloop()
