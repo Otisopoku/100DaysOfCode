2effc7f8b0a9f6ff783b74d716e0be4d460ab9e7
Otis Opoku <otisopoku30@gmail.com>
2026-01-15 22:38:45 +0000

    implement back card image flip

--- Day31/main.py
+++ Day31/main.py
@@ -1,19 +1,20 @@
 from tkinter import *
 import pandas as pd
 import random as r
+import time
 
 
 data_frame = pd.read_csv(filepath_or_buffer="data/french_words.csv", skiprows=0)
 data_dict = data_frame.to_dict(orient="records")
 
 
-    
-def handle_right_button_click():
+
+def handle_button_click():
     word_pair = r.choice(data_dict)
     french_word = word_pair["French"]
     canvas.itemconfig(canvas_word, text=french_word)
     canvas.itemconfig(canvas_title, text="French")
-    
+    canvas.itemconfig(main_canvas, image=back_card_image)
     
 
 BACKGROUND_COLOR = "#B1DDC6"
@@ -25,19 +26,21 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # canvas object enables us to lay objects ontop of each other
 canvas = Canvas(width=800, height=526)
 front_card_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 265, image=front_card_img)
+back_card_image = PhotoImage(file="images/card_back.png")
+main_canvas = canvas.create_image(400, 265, image=front_card_img)
 canvas_title = canvas.create_text(400, 150, text="French", font=("Arial", 40, "italic"))
 canvas_word = canvas.create_text(400, 263, text="donne", font=("Arial", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=handle_button_click)
 wrong_button.grid(row=1, column=0)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=handle_right_button_click)
+right_button = Button(image=right_image, highlightthickness=0, command=handle_button_click)
 right_button.grid(row=1, column=1)
+
 
 
 
