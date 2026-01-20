fc38208bf96f08baad79bc15cce26f5bb60483a3
Otis Opoku <otisopoku30@gmail.com>
2026-01-20 12:54:28 +0000

    flips card to display english word. Add timer

--- Day31/main.py
+++ Day31/main.py
@@ -1,27 +1,48 @@
 from tkinter import *
 import pandas as pd
 import random as r
-import time
 
+#NB: With tkiinter, we use the windows.after method to set timer. Using the time module ie: time.sleep will freeze the UI
+
+BACKGROUND_COLOR = "#B1DDC6"
 
 data_frame = pd.read_csv(filepath_or_buffer="data/french_words.csv", skiprows=0)
 data_dict = data_frame.to_dict(orient="records")
+word_pair = r.choice(data_dict)
+flip_timer = None # to handle and cancel timer in situations where the user clicks on the correct or wrong button before the timer ends so a new
+# 5 seconds timer will be initiated on the next card flip
 
 
 
-def handle_button_click():
+def show_new_card():
+    global word_pair, flip_timer 
+    
+    if flip_timer:
+        window.after_cancel(flip_timer)
+    
     word_pair = r.choice(data_dict)
     french_word = word_pair["French"]
     canvas.itemconfig(canvas_word, text=french_word)
     canvas.itemconfig(canvas_title, text="French")
-    canvas.itemconfig(main_canvas, image=back_card_image)
+    canvas.itemconfig(main_canvas, image=front_card_img)
     
+    flip_timer= window.after(5000, flip_card, word_pair)
 
-BACKGROUND_COLOR = "#B1DDC6"
+    
+def flip_card(word_pair):
+    if word_pair:
+        english_word = word_pair["English"]
+        canvas.itemconfig(canvas_title, text="English")
+        canvas.itemconfig(canvas_word, text=english_word)
+        canvas.itemconfig(main_canvas, image=back_card_image)
+
+
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+
 
 # canvas object enables us to lay objects ontop of each other
 canvas = Canvas(width=800, height=526)
@@ -34,14 +55,14 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, command=handle_button_click)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=show_new_card)
 wrong_button.grid(row=1, column=0)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=handle_button_click)
+right_button = Button(image=right_image, highlightthickness=0, command=show_new_card)
 right_button.grid(row=1, column=1)
 
 
-
+show_new_card()
 
 window.mainloop()
